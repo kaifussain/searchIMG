de8ec406eb0e9a57fbbefa1cbdc6353e024f493b
k417hussain <k417hussain@gmail.com>
2024-01-21 22:04:47 +0530

    Add more images feature enabled

--- search-img/src/App.js
+++ search-img/src/App.js
@@ -2,64 +2,92 @@ import "./App.css";
 import appLogo from "./resources/appLogo.png";
 import searchIcon from "./resources/searchIcon.png";
 import noProfile from "./resources/NoProfile.png";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import ImageC from "./Components/ImageC";
 
 function App() {
-  console.log('app start')
+  console.log("app start");
   const [darkMode, setDarkMode] = useState(true);
   const [userMode, setUserMode] = useState(false);
   const [imgSubj, setImgSubj] = useState("");
+  const [page, setPage] = useState(1);
   const [newInput, setNewInput] = useState("");
   const [fetchedImgs, setFetchedImgs] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchNew, setSearchNew] = useState(true);
+  const searchResultImgDiv = useRef(null);
+  const [scrollDown, setScrollDown] = useState(false);
+
   let localM = JSON.parse(localStorage.getItem("localM"));
-  let url = `https://api.unsplash.com/search/photos?page=1&query=${imgSubj}&client_id=${process.env.REACT_APP_API_KEY}`;
+  let url = `https://api.unsplash.com/search/photos?page=${page}&query=${imgSubj}&client_id=${process.env.REACT_APP_API_KEY}`;
+
+  useEffect(() => {
+    const myDiv = searchResultImgDiv.current;
+    const handleScroll = () => {
+      if (myDiv.scrollTop + myDiv.clientHeight > myDiv.scrollHeight - 200) {
+        setScrollDown(true);
+      } else {
+        setScrollDown(false);
+      }
+    };
+
+    myDiv.addEventListener("scroll", handleScroll);
+
+    return () => {
+      myDiv.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollDown]);
 
   useEffect(() => {
     if (!localM) {
       document.body.classList.add("lMode");
       setDarkMode(false);
     }
-    console.log('useEffect 1');
+    console.log("useEffect 1");
   }, []);
   function toggleDarkMode() {
     setDarkMode(!darkMode);
     document.body.classList.toggle("lMode", darkMode);
     localStorage.setItem("localM", JSON.stringify(!darkMode));
-    console.log('toggleDarkMode');
+    console.log("toggleDarkMode");
   }
-  
+
   async function fetchImgs() {
-    setFetchedImgs([])
+    if (searchNew) {
+      setFetchedImgs([]);
+      // setSearchNew(false);
+    }
     setLoading(true);
 
     const response = await fetch(url);
     const data = await response.json();
-    
-    setFetchedImgs(data.results);
-    console.log('fechImgs');
+
+    // setFetchedImgs(data.results);
+    setFetchedImgs(prev => [...prev,...data.results]);
+    console.log("fechImgs");
     setLoading(false);
+
   }
   function handleSubmit(e) {
     e.preventDefault();
-    if(newInput.trim() !== ''){
-
+    if (newInput.trim() !== "") {
+      setSearchNew(true);
+      setPage(1)
       setImgSubj(newInput);
-      setNewInput('');
+      setNewInput("");
     }
-      console.log('handleSubmit');
   }
-  useEffect(()=>{
-    if(imgSubj !== '')
-    fetchImgs();
-    console.log('useEffect 2');
-  },[imgSubj])
+  useEffect(() => {
+    if (imgSubj !== "") fetchImgs();
+  }, [imgSubj, page]);
 
-  function newImageFetch() {
-    fetchImgs();
+  function addMoreImgs() {
+    document.getElementById('moreBtn').classList.add('hideMoreBtn');
+    setSearchNew(false);
+    setPage((prev) => prev + 1);
+
   }
-  console.log('app end')
+  console.log("app end");
 
   return (
     <div>
@@ -72,8 +100,8 @@ function App() {
             id="searchInput"
             placeholder="Search for an image"
             className={darkMode ? "" : "lMode"}
-            onChange={(e)=>{
-              setNewInput(e.target.value)
+            onChange={(e) => {
+              setNewInput(e.target.value);
             }}
           ></input>
           <button id="searchBtn">
@@ -121,36 +149,64 @@ function App() {
           <div
             className="suggestionItem"
             onClick={() => {
-              setImgSubj("3D render");
-              newImageFetch();
+              setSearchNew(true);
+              setImgSubj("Wallpaper");
             }}
           >
-            3D render
+            Wallpaper
           </div>
-          <div className="suggestionItem">school</div>
-          <div className="suggestionItem">tokyo</div>
-          <div className="suggestionItem">AI art</div>
+          <div
+            className="suggestionItem"
+            onClick={() => {
+              setSearchNew(true);
+              if (imgSubj !== "Minimal") setImgSubj("Minimal");
+            }}
+            >
+            Minimal
+          </div>
+          <div
+            className="suggestionItem"
+            onClick={() => {
+              setSearchNew(true);
+              if (imgSubj !== "Asthetic") setImgSubj("Asthetic");
+            }}
+            >
+            Asthetic
+          </div>
+          <div
+            className="suggestionItem"
+            onClick={() => {
+              setSearchNew(true);
+              if (imgSubj !== "AI art") setImgSubj("AI art");
+            }}
+          >
+            AI art
+          </div>
         </div>
       </div>
 
-      <div id="searchResultDiv">
-        <div id='loading' className={loading?'':'hide'}></div>
-        <div
-          id="searchResultTitle"
-          // imgSubj ? "fromLeftAni" : "hide" & 
-          className={darkMode ? "" : "lMode"}
-        >
+      <div id="searchResultDiv" ref={searchResultImgDiv}>
+        <div id="loading" className={loading ? "" : "hide"}></div>
+        <div id="searchResultTitle" className={darkMode ? "" : "lMode"}>
           <span id="searchResultTitleName">{imgSubj}</span>
         </div>
         <div id="searchResultImgDiv">
-          
-          {fetchedImgs.length > 0 ? (
-            fetchedImgs.map((img) => {
-              return <ImageC src={img.urls.small} />;
-            })
-          ) : (
-            loading == false && <div style={{ marginTop:'10px'}}>Nothing here!</div>
-          )}
+          {fetchedImgs.length > 0
+            ? fetchedImgs.map((img, i) => {
+                return <ImageC key={page + "-" + i} src={img.urls.small} />;
+              })
+            : loading == false && (
+                <div style={{ marginTop: "10px" }}>Nothing here!</div>
+              )}
+          <button
+            id="moreBtn"
+            className={`${darkMode ? "" : "lMode"} ${
+              scrollDown ? "" : "hideMoreBtn"
+            }`}
+            onClick={addMoreImgs}
+          >
+            +
+          </button>
         </div>
       </div>
     </div>
